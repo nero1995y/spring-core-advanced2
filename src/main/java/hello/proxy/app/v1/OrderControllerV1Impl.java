fa5d4ee270656d8a21d583b76356fa85d6902967
nero <wnsgur765z@naver.com>
2022-07-14 11:44:34 +0900

    feat: Add v1 controller @Slf4j

--- src/main/java/hello/proxy/app/v1/OrderControllerV1Impl.java
+++ src/main/java/hello/proxy/app/v1/OrderControllerV1Impl.java
@@ -1,5 +1,8 @@
 package hello.proxy.app.v1;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class OrderControllerV1Impl implements OrderControllerV1{
 
     private final OrderServiceV1 orderService;
