707b4452d01be3b3bcbc05b8419de6731e7cc943
nero <wnsgur765z@naver.com>
2022-07-17 19:26:25 +0900

    feat: Add proxy 적용

---
+++ src/test/java/hello/proxy/pureporxy/concreatepoxy/code/ConcreteClient.java
@@ -0,0 +1,15 @@
+package hello.proxy.pureporxy.concreatepoxy.code;
+
+
+public class ConcreteClient {
+
+    private ConcreteLogic concreteLogic;
+
+    public ConcreteClient(ConcreteLogic concreteLogic) {
+        this.concreteLogic = concreteLogic;
+    }
+
+    public void execute() {
+        concreteLogic.operation();
+    }
+}
